6cc26649c103a34cd9144c1ac82e265b72d935aa
sanyokstd <sanyokstd@gmail.com>
2022-02-07 17:56:50 +0200

    1
    1

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,28 @@
 "use strict";
 "use strict";
 
-var person__menu__btn = document.getElementById('person__menu__btn');
-var person = document.querySelector('.person');
-person__menu__btn.addEventListener('click', function () {
-  person.classList.toggle('open');
-});
-document.addEventListener('click', function (e) {
-  if (!e.target.closest('.person')) {
-    person.classList.remove('open');
-  }
-});
-var openSearch = document.getElementById('open-search');
-openSearch.addEventListener('click', function () {
-  openSearch.classList.toggle('active');
-  document.querySelector('.header__search').classList.toggle('active');
+$(function () {
+  var person__menu__btn = document.getElementById('person__menu__btn');
+  var person = document.querySelector('.person');
+  person__menu__btn.addEventListener('click', function () {
+    person.classList.toggle('open');
+  });
+  document.addEventListener('click', function (e) {
+    if (!e.target.closest('.person')) {
+      person.classList.remove('open');
+    }
+  });
+  var openSearch = document.getElementById('open-search');
+  openSearch.addEventListener('click', function () {
+    openSearch.classList.toggle('active');
+    document.querySelector('.header__search').classList.toggle('active');
+  });
+  Inputmask().mask(document.querySelectorAll(".mask"));
+  $('.search-input input').on('focus', function () {
+    $(this).closest('.search-input').addClass('focus');
+  });
+  $('.search-input input').on('blur', function () {
+    $(this).closest('.search-input').removeClass('focus');
+  });
 });
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,32 @@
-const person__menu__btn = document.getElementById('person__menu__btn')
-const person = document.querySelector('.person')
-person__menu__btn.addEventListener('click', function(){
-    person.classList.toggle('open')
-})
-
-document.addEventListener('click', (e) => {
+$(function(){
+    const person__menu__btn = document.getElementById('person__menu__btn')
+    const person = document.querySelector('.person')
+    person__menu__btn.addEventListener('click', function(){
+        person.classList.toggle('open')
+    })
     
-    if(!e.target.closest('.person')){
-        person.classList.remove('open')
-    }
+    document.addEventListener('click', (e) => {
+        
+        if(!e.target.closest('.person')){
+            person.classList.remove('open')
+        }
+    })
+    
+    const openSearch = document.getElementById('open-search')
+    
+    openSearch.addEventListener('click', ()=>{
+        openSearch.classList.toggle('active')
+        document.querySelector('.header__search').classList.toggle('active')
+    })
+    
+    Inputmask().mask(document.querySelectorAll(".mask"));
+
+    $('.search-input input').on('focus', function(){
+        $(this).closest('.search-input').addClass('focus')
+    })
+
+    $('.search-input input').on('blur', function(){
+        $(this).closest('.search-input').removeClass('focus')
+    })
 })
 
-const openSearch = document.getElementById('open-search')
-
-openSearch.addEventListener('click', ()=>{
-    openSearch.classList.toggle('active')
-    document.querySelector('.header__search').classList.toggle('active')
-})
